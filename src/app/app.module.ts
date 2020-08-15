547f42938146d3760cd182d0c257facbac901a02
priyanka6065 <priyankapatel.it@gmail.com>
2020-08-15 00:32:40 -0400

    new rentalappdemo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AddPropertyComponent } from './add-property/add-property.component';
+import { PropertyCardComponent } from './property-card/property-card.component';
+import { PropertyListComponent } from './property-list/property-list.component';
+import { SearchComponent } from './search/search.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
+
+const appRoutes: Routes = [
+{ path: '' , component:PropertyListComponent },
+{ path: 'buy' , component:PropertyListComponent },
+{ path: 'rent' , component:PropertyListComponent },
+{ path: 'login' , component:UserLoginComponent },
+{ path: 'register' , component:UserRegistrationComponent },
+{ path: 'app-property' , component:AddPropertyComponent }
+] ;
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    AddPropertyComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+    SearchComponent,
+    UserLoginComponent,
+    UserRegistrationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
